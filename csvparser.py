2d9850127a83486d33076804776a391794df793e
Ansh <lightnarcissus@gmail.com>
2016-11-04 08:04:59 -0400

    Before setup

--- csvparser.py
+++ csvparser.py
@@ -2,4 +2,4 @@ import csv
 with open('greenhouse.csv', 'rb') as f:
     r = csv.DictReader(f, delimiter=',', skipinitialspace=True)
     for row in r:
-    	print "['"  +row['Country Code'] + "'," + row['2012'] + "],"
+    	print "'"  +row['Country Code'] + "',"
